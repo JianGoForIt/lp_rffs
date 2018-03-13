879545a32b39a63ce234a6abea0af357dab3c281
AnonRepository <anonrepository@gmail.com>
2018-03-13 11:46:20 -0700

    add folder check for scripts

--- kernel_reg/run_sweep_kernel_ridge_regression.sh
+++ kernel_reg/run_sweep_kernel_ridge_regression.sh
@@ -4,19 +4,29 @@ output=${3}
 # n_fp_rff=${4}
 sigma=30.0
 # run exact kernel
-python rff_kernel_census.py --exact_kernel --sigma=${sigma} --reg_lambda=${lambda} --output_folder="${output}_exact"
+if [ ! -f "${output}_exact/results.pkl" ]; then
+  echo "${output}_exact/results.pkl"
+  python rff_kernel_census.py --exact_kernel --sigma=${sigma} --reg_lambda=${lambda} --output_folder="${output}_exact"
+fi
 
-for n_fp_rff in 64 128 256 512 1024 2048 4096 8192 16384
+#for n_fp_rff in 64 128 256 512 1024 2048 4096 8192 16384
+for n_fp_rff in 32 
   do 
     # run fp rff kernel
-    python rff_kernel_census.py --do_fp --n_fp_rff=${n_fp_rff} --sigma=${sigma} --random_seed=${seed} --reg_lambda=${lambda} --output_folder="${output}_n_fp_feat_${n_fp_rff}_fp_rff"
+    if [ ! -f "${output}_n_fp_feat_${n_fp_rff}_fp_rff/results.pkl" ]; then
+      echo "${output}_n_fp_feat_${n_fp_rff}_fp_rff"
+      python rff_kernel_census.py --do_fp --n_fp_rff=${n_fp_rff} --sigma=${sigma} --random_seed=${seed} --reg_lambda=${lambda} --output_folder="${output}_n_fp_feat_${n_fp_rff}_fp_rff"
+    fi    
 
     # run lp rff kernel
     for nbit in 32 16 8 4 2 1
       do
-        python rff_kernel_census.py --n_bit=${nbit} --n_fp_rff=${n_fp_rff} --sigma=${sigma} --random_seed=${seed} --reg_lambda=${lambda} --output_folder="${output}_nbit_${nbit}_n_fp_feat_${n_fp_rff}_lp_rff"
+	if [ ! -f "${output}_nbit_${nbit}_n_fp_feat_${n_fp_rff}_lp_rff/results.pkl" ]; then
+          echo "${output}_nbit_${nbit}_n_fp_feat_${n_fp_rff}_lp_rff"
+          python rff_kernel_census.py --n_bit=${nbit} --n_fp_rff=${n_fp_rff} --sigma=${sigma} --random_seed=${seed} --reg_lambda=${lambda} --output_folder="${output}_nbit_${nbit}_n_fp_feat_${n_fp_rff}_lp_rff"
+        fi 
       done
-  done
+   done
 
 # for lambda in 1e-6 1e-5 1e-4 1e-3 1e-2 1e-1 1e0 1e1
 #   do
