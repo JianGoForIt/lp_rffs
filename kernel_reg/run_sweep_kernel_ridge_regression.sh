8d088a2893079e713d633c09dffb7234eaef75dd
AnonRepository <anonrepository@gmail.com>
2018-03-22 17:39:07 -0700

    update script to avoid duplicated configs

--- kernel_reg/run_sweep_kernel_ridge_regression.sh
+++ kernel_reg/run_sweep_kernel_ridge_regression.sh
@@ -4,33 +4,25 @@ output=${3}
 # n_fp_rff=${4}
 sigma=30.0
 # run exact kernel
-python rff_kernel_census.py --exact_kernel --sigma=${sigma} --reg_lambda=${lambda} --output_folder="${output}_exact"
+if [ ! -f "${output}_exact/results.pkl" ]; then
+  echo "${output}_exact/results.pkl"
+  python rff_kernel_census.py --exact_kernel --sigma=${sigma} --reg_lambda=${lambda} --output_folder="${output}_exact"
+fi
 
-for n_fp_rff in 32 64 128 256 512 1024 2048 4096 8192
+for n_fp_rff in 4096
   do 
     # run fp rff kernel
-    python rff_kernel_census.py --do_fp --n_fp_rff=${n_fp_rff} --sigma=${sigma} --random_seed=${seed} --reg_lambda=${lambda} --output_folder="${output}_n_fp_feat_${n_fp_rff}_fp_rff"
-
+    if [ ! -f "${output}_n_fp_feat_${n_fp_rff}_fp_rff/results.pkl" ]; then
+      echo "${output}_n_fp_feat_${n_fp_rff}_fp_rff"
+      python rff_kernel_census.py --do_fp --n_fp_rff=${n_fp_rff} --sigma=${sigma} --random_seed=${seed} --reg_lambda=${lambda} --output_folder="${output}_n_fp_feat_${n_fp_rff}_fp_rff"
+    fi
+    
     # run lp rff kernel
-    for nbit in 1 2 4 8 16 32
+    for nbit in 1
       do
-        python rff_kernel_census.py --n_bit=${nbit} --n_fp_rff=${n_fp_rff} --sigma=${sigma} --random_seed=${seed} --reg_lambda=${lambda} --output_folder="${output}_nbit_${nbit}_n_fp_feat_${n_fp_rff}_lp_rff" --test_var_reduce
+        if [ ! -f "${output}_nbit_${nbit}_n_fp_feat_${n_fp_rff}_lp_rff/results.pkl" ]; then
+          echo "${output}_nbit_${nbit}_n_fp_feat_${n_fp_rff}_lp_rff"
+          python rff_kernel_census.py --n_bit=${nbit} --n_fp_rff=${n_fp_rff} --sigma=${sigma} --random_seed=${seed} --reg_lambda=${lambda} --output_folder="${output}_nbit_${nbit}_n_fp_feat_${n_fp_rff}_lp_rff" --test_var_reduce
+        fi
       done
   done
-
-# for lambda in 1e-6 1e-5 1e-4 1e-3 1e-2 1e-1 1e0 1e1
-#   do
-#     python rff_kernel_census.py --exact_kernel --sigma=${sigma} --reg_lambda=${lambda}
-#   done
-
-# for n_fp_rff in 64 128 256 512 1024 2048 4096 8192 16384 
-#   do
-#     for lambda in 1e-6 1e-5 1e-4 1e-3 1e-2 1e-1 1e0 1e1
-#       do
-#         python rff_kernel_census.py --do_fp --n_fp_rff=${n_fp_rff} --sigma=${sigma} --random_seed=${seed} --reg_lambda=${lambda}
-#         for nbit in 32 16 8 4 2 1
-#           do
-#             python rff_kernel_census.py --n_bit=${nbit} --n_fp_rff=${n_fp_rff} --sigma=${sigma} --random_seed=${seed} --reg_lambda=${lambda}
-#           done
-#       done
-#   done
