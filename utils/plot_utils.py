414a35005d8bc9562b887026c997c2090a5e5d67
zjian <zjian@stanford.edu>
2018-03-29 12:52:02 -0700

    plot utils

---
+++ utils/plot_utils.py
@@ -0,0 +1,13 @@
+import matplotlib
+import matplotlib.pyplot as plt
+
+def get_colors():
+  prop_cycle = plt.rcParams['axes.prop_cycle']
+  colors = prop_cycle.by_key()['color']
+  colors_dict = {}
+  colors_dict["exact"] = colors[0]
+  colors_dict["fp"] = colors[1]
+  for idx, nbit in enumerate([1,2,4,8,16,32] ):
+    colors_dict[str(nbit)] = colors[idx + 2]
+  #print colors_dict
+  return colors_dict
