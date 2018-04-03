4a1f717825cc35cd2bce921468c4c3a9019dff1e
zjian <zjian@stanford.edu>
2018-04-02 22:15:26 -0700

    pca rff script first 1024 then 8192 features

---
+++ kernel_reg/meta_run_sweep_kernel_ridge_regression_pca_rff.sh
@@ -0,0 +1,12 @@
+# seed=${1}
+# output=${2}
+# lambda=${3}
+# n_fp_rff=${4}
+for seed in 1 2 3 4 5 #6 7 8 9 10
+  do 
+    for lambda in 1e-6 1e-5 1e-4 1e-3 1e-2 1e-1 1e0 1e1 1e2 
+      do
+        bash run_sweep_kernel_ridge_regression_pca_rff.sh ${seed} ${lambda} /dfs/scratch0/zjian/lp_kernel/census_results_64_bit_pca_rff/lambda_${lambda}_seed_${seed} & 
+        # bash run_sweep_kernel_ridge_regression_pca_rff.sh ${seed} ${lambda} ./test/census_results_64_bit_pca_rff/lambda_${lambda}_seed_${seed}
+      done
+  done
