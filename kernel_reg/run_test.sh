3834aaecb49be9f65bfdb03b33b6855fa81dda3d
AnonRepository <anonrepository@gmail.com>
2018-04-06 17:57:25 -0700

    change run_test

--- kernel_reg/run_test.sh
+++ kernel_reg/run_test.sh
@@ -7,13 +7,18 @@
 # python rff_kernel_census.py --do_fp --n_fp_rff=16384 --sigma=28.87 --reg_lambda=0.005
 # python rff_kernel_census.py --n_bit=8 --n_fp_rff=512 --sigma=10.0 --reg_lambda=10.0
 
-
-# test auto pca quantizer
-python rff_kernel_census.py --pca_rff --pca_rff_auto_scale --pca_rff_perc=0.0\
+python rff_kernel_census.py --pca_rff --pca_rff_mu=10.0 \
   --n_fp_rff=128 --pca_rff_n_base_fp_feat=1024 --sigma=30.0 --random_seed=1 --reg_lambda=1e-6
 
-python rff_kernel_census.py --pca_rff --pca_rff_auto_scale --pca_rff_perc=10.0\
+python rff_kernel_census_bak.py --pca_rff --pca_rff_mu=10.0 \
   --n_fp_rff=128 --pca_rff_n_base_fp_feat=1024 --sigma=30.0 --random_seed=1 --reg_lambda=1e-6
 
-python rff_kernel_census.py --pca_rff --pca_rff_auto_scale --pca_rff_perc=10.0\
-  --n_fp_rff=128 --pca_rff_n_base_fp_feat=1024 --sigma=30.0 --random_seed=1 --reg_lambda=1e-6
+## test auto pca quantizer
+#python rff_kernel_census.py --pca_rff --pca_rff_auto_scale --pca_rff_perc=0.0\
+#  --n_fp_rff=128 --pca_rff_n_base_fp_feat=1024 --sigma=30.0 --random_seed=1 --reg_lambda=1e-6
+#
+#python rff_kernel_census.py --pca_rff --pca_rff_auto_scale --pca_rff_perc=10.0\
+#  --n_fp_rff=128 --pca_rff_n_base_fp_feat=1024 --sigma=30.0 --random_seed=1 --reg_lambda=1e-6
+#
+#python rff_kernel_census.py --pca_rff --pca_rff_auto_scale --pca_rff_perc=10.0\
+#  --n_fp_rff=128 --pca_rff_n_base_fp_feat=1024 --sigma=30.0 --random_seed=1 --reg_lambda=1e-6
