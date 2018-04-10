17aa6b367fd91d81272cf2a1882d7f4ac93d4451
AnonRepository <anonrepository@gmail.com>
2018-04-09 21:40:33 -0700

    correct setup with eigh for eigen decompostion

--- kernel_reg/meta_run_sweep_kernel_ridge_regression_fix_design.sh
+++ kernel_reg/meta_run_sweep_kernel_ridge_regression_fix_design.sh
@@ -2,7 +2,7 @@
 # output=${2}
 # lambda=${3}
 # n_fp_rff=${4}
-for seed in 1 2 3 #6 7 8 9 10
+for seed in 3 #6 7 8 9 10
   do 
     for lambda in 1e-6 # 1e-5 1e-4 1e-3 1e-2 1e-1 1e0 1e1 1e2 
       do
