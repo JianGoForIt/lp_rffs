b51ea8a309c60a3112c51ae3f09f714a88b3eb43
AnonRepository <anonrepository@gmail.com>
2018-04-21 14:51:24 -0700

    fix random seed issue for codrna

--- models/run_lp_model_fp_feat_codrna.sh
+++ models/run_lp_model_fp_feat_codrna.sh
@@ -8,7 +8,7 @@ for seed in 1 2 3
         python run_model.py --model=logistic_regression --minibatch=250 --l2_reg=0.0 \
           --kernel_sigma=30.0 --n_fp_rff=10000 --random_seed=${seed} --do_fp_feat --learning_rate=${lr} \
           --data_path=/dfs/scratch0/zjian/data/lp_kernel_data/codrna --opt=sgd --epoch=100 --do_fp_model --cuda \
-          --save_path=/dfs/scratch0/zjian/lp_kernel/codrna_results_sgd_lp_model_fp_feat/sgd_lr_${lr}_seed_${seed}
+          --save_path=/dfs/scratch0/zjian/lp_kernel/codrna_results_sgd_lp_model_fp_feat/fpsgd_lr_${lr}_seed_${seed}
       done
     for model_bit in ${model_bit_in} #8 #16 4
       do
@@ -27,7 +27,7 @@ for seed in 1 2 3
             for mu in 1e-3 1e-2 1e-1 1e0 1e1 1e2 1e3 1e4
               do
                 python run_model.py --model=logistic_regression --minibatch=250 --l2_reg=0.0 \
-                  --kernel_sigma=30.0 --n_fp_rff=10000 --random_seed=2 --do_fp_feat --learning_rate=${lr} \
+                  --kernel_sigma=30.0 --n_fp_rff=10000 --random_seed=${seed} --do_fp_feat --learning_rate=${lr} \
                   --data_path=/dfs/scratch0/zjian/data/lp_kernel_data/codrna --opt=halp --epoch=100 \
                   --halp_mu=${mu} --halp_epoch_T=2.0 --cuda --n_bit_model=${model_bit} \
                   --save_path=/dfs/scratch0/zjian/lp_kernel/codrna_results_sgd_lp_model_fp_feat/halp_lr_${lr}_model_bits_${model_bit}_mu_${mu}_epoch_T_2.0_seed_${seed}
