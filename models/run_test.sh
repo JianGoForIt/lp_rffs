957c5aa9460bddd8d6353d805df44723df93518a
zjian <zjian@stanford.edu>
2018-04-21 10:55:12 -0700

    sync script

--- models/run_test.sh
+++ models/run_test.sh
@@ -6,6 +6,12 @@
 #   --kernel_sigma=2.24 --n_fp_rff=10000 --random_seed=2 --do_fp_feat --learning_rate=100.0 \
 #   --data_path=../../data/adult/adult --opt=sgd --epoch=5
 
+python run_model.py --model=logistic_regression --minibatch=250 --l2_reg=0.0 \
+  --kernel_sigma=2.24 --n_fp_rff=10000 --random_seed=2 --do_fp_feat --learning_rate=100.0 \
+  --data_path=../../data/adult/adult --opt=lpsgd --epoch=1 --n_bit_model=2 --scale_model=0.001 \
+  --save_path=./test/123/
+
+
 # python run_model.py --model=logistic_regression --minibatch=250 --l2_reg=0.0 \
 #   --kernel_sigma=2.24 --n_fp_rff=10000 --random_seed=2 --do_fp_feat --learning_rate=100.0 \
 #   --data_path=../../data/adult/adult --opt=lpsgd --epoch=1 --n_bit_model=2 --scale_model=0.001
@@ -19,10 +25,10 @@
 #   --kernel_sigma=2.24 --n_fp_rff=10000 --random_seed=2 --n_bit_feat=4 --learning_rate=100.0 \
 #   --data_path=../../data/adult/adult --opt=lpsgd --epoch=1 --n_bit_model=2 --scale_model=0.001
 
-python run_model.py --model=logistic_regression --minibatch=250 --l2_reg=0.0 \
-  --kernel_sigma=2.24 --n_fp_rff=10000 --random_seed=2 --n_bit_feat=4 --learning_rate=100.0 \
-  --data_path=../../data/adult/adult --opt=halp --epoch=1 --n_bit_model=2 --halp_mu=10.0 \
-  --halp_epoch_T=1.0
+# python run_model.py --model=logistic_regression --minibatch=250 --l2_reg=0.0 \
+#   --kernel_sigma=2.24 --n_fp_rff=10000 --random_seed=2 --n_bit_feat=4 --learning_rate=100.0 \
+#   --data_path=../../data/adult/adult --opt=halp --epoch=1 --n_bit_model=2 --halp_mu=10.0 \
+#   --halp_epoch_T=1.0
 
 # python run_model.py --model=logistic_regression --minibatch=250 --l2_reg=0.0 \
 #   --kernel_sigma=2.24 --n_fp_rff=10000 --random_seed=2 --n_bit_feat=2 --learning_rate=100.0 \
