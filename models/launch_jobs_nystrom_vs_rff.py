5adb05f0ba85dbceda6789f0fb39ac7c0a2969e4
AnonRepository <anonrepository@gmail.com>
2018-04-28 02:18:44 -0700

    add launch job script which works for both starcluster and dawn

--- models/launch_jobs_nystrom_vs_rff.py
+++ models/launch_jobs_nystrom_vs_rff.py
@@ -2,26 +2,44 @@ import os, sys
 import math
 from copy import deepcopy
 
-# /dfs/scratch0/zjian/data/lp_kernel_data/census
-template = "python /dfs/scratch0/zjian/lp_kernel_code/lp_kernel/models/run_model.py --model=unk --minibatch=250 --l2_reg=unk \
-  --kernel_sigma=unk --n_fp_rff=unk --random_seed=unk --learning_rate=unk \
-  --data_path=unk --opt=unk --epoch=unk \
-  --save_path=unk --approx_type=unk --cuda"
 
 #dataset = "census"
 #exp_name = "nystrom_vs_rff"
 #approx_type = "rff"
 dataset = sys.argv[1]
 exp_name = sys.argv[2]
+cluster = sys.argv[3]  # starcluster / dawn
+do_metric = sys.argv[4] # with_metric / without_metric
 #approx_type = sys.argv[3]
+
+# /dfs/scratch0/zjian/data/lp_kernel_data/census
+if cluster == "starcluster":
+    template = "python /dfs/scratch0/zjian/lp_kernel_code/lp_kernel/models/run_model.py --model=unk --minibatch=250 --l2_reg=unk \
+        --kernel_sigma=unk --n_fp_rff=unk --random_seed=unk --learning_rate=unk \
+        --data_path=unk --opt=unk --epoch=unk \
+        --save_path=unk --approx_type=unk --cuda"
+
+else:
+    template = "python /lfs/1/zjian/lp_kernel/lp_kernel/models/run_model.py --model=unk --minibatch=250 --l2_reg=unk \
+        --kernel_sigma=unk --n_fp_rff=unk --random_seed=unk --learning_rate=unk \
+        --data_path=unk --opt=unk --epoch=unk \
+        --save_path=unk --approx_type=unk"
+
 
 if dataset == "census":
     model = "ridge_regression"
-    l2_reg_list = [0, 1e-6, 1e-5, 1e-4, 1e-3, 1e-2, 1e-1]
+    if do_metric == "with_metric":
+	l2_reg_list = [0.0, ]
+    else:
+        l2_reg_list = [0, 1e-6, 1e-5, 1e-4, 1e-3, 1e-2, 1e-1]
     kernel_sigma = math.sqrt(1.0/0.0006/2.0)
-    n_fp_rff_list = [1250, 2500, 5000, 10000, 20000, 50000, 100000, 200000, 400000, 800000, 1600000]
+    #n_fp_rff_list = [1250, 5000, 20000, 100000, 400000, 1600000]
+    n_fp_rff_list = [2500, 10000, 50000, 200000, 800000]
     seed_list = [1, 2, 3]
-    lr_list = [0.5, 0.1, 1.0]
+    if do_metric == "with_metric":
+	lr_list = [0.5]
+    else:
+        lr_list = [0.5, 0.1, 1.0]
     data_path = "/dfs/scratch0/zjian/data/lp_kernel_data/" + dataset
     opt = "sgd"
     epoch = 300
@@ -47,15 +65,22 @@ for seed in seed_list:
 					command = command.replace("--epoch=unk", "--epoch="+str(epoch) )
 					command = command.replace("--save_path=unk", "--save_path="+save_path + save_suffix)
 					command = command.replace("--approx_type=unk", "--approx_type="+approx_type)
+					command += " --collect_sample_metrics"
 					os.system("mkdir -p " + save_path + save_suffix)
-					command = "cd /dfs/scratch0/zjian/lp_kernel_code/lp_kernel/models && " + command
+					if cluster == "starcluster":
+						command = "cd /dfs/scratch0/zjian/lp_kernel_code/lp_kernel/models && " + command
+					else:
+						command = "cd /lfs/1/zjian/lp_kernel/lp_kernel/models && " + command
 					f = open(save_path + save_suffix + "/job.sh", "w")
 					f.write(command)
 					f.close()
-					launch_command = "qsub -V " \
+					if cluster == "starcluster":
+						launch_command = "qsub -V " \
 						+ " -o " + save_path + save_suffix + "/run.log " \
                                 	        + " -e " + save_path + save_suffix + "/run.err " + save_path + save_suffix + "/job.sh"
-					if sys.argv[3] == "dryrun":
+					else:
+						launch_command = "bash " + save_path + save_suffix + "/job.sh"
+					if sys.argv[5] == "dryrun":
 						print(launch_command)
 					else:
 						print(launch_command)	
