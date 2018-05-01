2489b273097b720bc779a01b3fe91b8da9386ea8
AnonRepository <anonrepository@gmail.com>
2018-05-01 02:51:42 -0700

    add fixed design closenees script for exact kernel on dawn

--- models/run_fixed_design_closeness.sh
+++ models/run_fixed_design_closeness.sh
@@ -3,5 +3,5 @@ for sigma in 1e4 #1e3 1e2 1e5
     python run_model.py --model=ridge_regression  \
       --kernel_sigma=28.8675134595 --random_seed=1  \
       --data_path=/dfs/scratch0/zjian/data/lp_kernel_data/census --save_path=/dfs/scratch0/zjian/lp_kernel/closeness/fixed_design/exact_auto_l2_reg_noise_sigma_${sigma} --approx_type=exact \
-      --do_fp_feat --collect_sample_metrics --fixed_design --fixed_design_auto_l2_reg --fixed_design_noise_sigma=${sigma}
+      --collect_sample_metrics --fixed_design --fixed_design_auto_l2_reg --fixed_design_noise_sigma=${sigma}
   done
