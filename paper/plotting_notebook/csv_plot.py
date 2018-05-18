fb014eaa8c9a0dd1de71a0bb5c76b782bd46f829
zjian <zjian@stanford.edu>
2018-05-18 05:01:22 -0700

    before ckpt sending to CR

--- paper/plotting_notebook/csv_plot.py
+++ paper/plotting_notebook/csv_plot.py
@@ -55,7 +55,7 @@ def plot_figure_with_error_bar(names, data, color_list):
     each column of data is a line
     the name follows the pattern like ['fp Nystrom-x', 'fp Nystrom-y', 'fp Nystrom-y_std', 'fp RFF-x', 'fp RFF-y', 'fp RFF-y_std']
     '''
-    marker_list = ['o', 'v', '^', 's', 'h', 'd', '+']
+    marker_list = ['v', 'd', 'o', 's', '+', 'v', 'h']
     for i in range(data.shape[1] // 3):
         idx = i * 3
         label = names[idx].split("|")[0]
@@ -63,8 +63,13 @@ def plot_figure_with_error_bar(names, data, color_list):
         x = data[:, idx]
         average_y = data[:, idx + 1]
         std_y = data[:, idx + 2]
-#         print x, average_y, std_y   
-        plt.errorbar(x, average_y, yerr=std_y, label=label, marker=marker_list[i], markeredgecolor=color_list[i % len(color_list)], fmt="-o", linewidth=1, capsize=3.5, capthick=1, color=color_list[i % len(color_list)])
+#         print x, average_y, std_y
+        if "FP" in label:
+            print "FP mode plot"
+            plt.errorbar(x, average_y, yerr=std_y, label=label, marker=marker_list[i], markeredgecolor=color_list[i % len(color_list)], markerfacecolor="none", markersize=8, markeredgewidth=1.5, fmt="-", linewidth=1, capsize=5, capthick=1, color=color_list[i % len(color_list)])
+        else:
+            print "LP mode plot"   
+            plt.errorbar(x, average_y, yerr=std_y, label=label, marker=marker_list[i], markeredgecolor=color_list[i % len(color_list)], markerfacecolor="none", markersize=8, markeredgewidth=1.5, fmt="--", linewidth=1, capsize=5, capthick=1, color=color_list[i % len(color_list)])
 
 def set_fig_xtick(values, labels, fontsize):
     ax = plt.gca()
