53ab2ecc339d18ca8a4456cf1a15b3371db84f52
zjian <zjian@stanford.edu>
2018-05-31 12:03:42 -0700

    clean file structures for release branch

--- kernel_reg/run_noise_trade_off.sh
+++
@@ -1,3 +0,0 @@
-python rff_kernel_census_noise_trade_off.py --fixed_design --fixed_design_opt_reg --fixed_design_data_sample_int=1 --fixed_design_noise_level=1e0 --n_fp_rff=4096 --sigma=30.0 --random_seed=1 --do_fp --output_folder=./test/noise_trade_off
-
-python rff_kernel_census_noise_trade_off.py --fixed_design --fixed_design_opt_reg --fixed_design_data_sample_int=1 --fixed_design_noise_level=1e0 --n_fp_rff=1024 --sigma=30.0 --random_seed=1 --do_fp --output_folder=./test/noise_trade_off
